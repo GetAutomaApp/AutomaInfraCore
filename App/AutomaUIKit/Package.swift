f74a2652107218c19f92b8a095a06b2172fe08f9
AdonisCodes <business@simonferns.com>
2024-11-05 04:45:28 -0800

    .

---
+++ App/AutomaUIKit/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:6
+
+import PackageDescription
+
+let package = Package(
+    name: "AutomaUIKit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "AutomaUIKit",
+            targets: ["AutomaUIKit"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "AutomaUIKit",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "AutomaUIKitTests",
+            dependencies: ["AutomaUIKit"],
+            path: "Tests"
+        ),
+    ]
+)
