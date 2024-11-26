5ce0c21207af12d2af74fcb92e5050faec2aa5bd
AdonisCodes <business@simonferns.com>
2024-11-26 05:05:25 -0800

    linting + custom title implementation details

--- App/AutomaUIKit/Sources/AutomaUIKit/Components/Texts/InfoPairComponent/InfoPairComponent_Previews.swift
+++ App/AutomaUIKit/Sources/AutomaUIKit/Components/Texts/InfoPairComponent/InfoPairComponent_Previews.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct InfoPairComponent_Previews: PreviewProvider {
     static var previews: some View {
         InfoPairComponent()
+        
+        
     }
-    
-    
-    
 }
