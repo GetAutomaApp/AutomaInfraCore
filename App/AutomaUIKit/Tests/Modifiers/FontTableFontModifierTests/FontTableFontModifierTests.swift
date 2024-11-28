7f02d33f20db29ccd6be88a58006c95411cb6d4c
AdonisCodes <business@simonferns.com>
2024-11-28 05:32:55 -0800

    fix spec

--- App/AutomaUIKit/Tests/Modifiers/FontTableFontModifierTests/FontTableFontModifierTests.swift
+++ App/AutomaUIKit/Tests/Modifiers/FontTableFontModifierTests/FontTableFontModifierTests.swift
@@ -11,21 +11,11 @@ import XCTest
 @testable import AutomaUIKit
 
 class FontTableFontComponentTests: XCTestCase {
-    // one test case per method
-    // one test case per completed flow (action)
-    // one test case per potential edge-case
-    @MainActor func testIntegration() throws {
-        // let component = Text("HI").__crimsonFont()
-        // XCTAssertNotNil(component)
-    }
-
     // Make sure the correct colour is applied
     @MainActor func testColour() throws {
         let color = DesignTokens.colors.primaryText
 
         let component = Text("HI").fontTableFont(FontTable.Body.body1, color)
-
-        let sut = try component.inspect()
     }
 
     // Make sure the custom font input works
