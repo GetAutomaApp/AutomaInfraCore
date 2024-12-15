af8434725d57edd8d24d501607b5e98940cc5c11
AdonisCodes <business@simonferns.com>
2024-12-14 17:28:34 -0800

    project cleanup

--- Backend/Sources/App/routes.swift
+++ Backend/Sources/App/routes.swift
@@ -12,8 +12,9 @@ func routes(_ app: Application) throws {
         "It works!"
     }
 
-    app.get("hello") { _ async -> String in
-        "Hello, world!"
+    app.get("hello") { req async -> String in
+        try await Todo().save(on: req.dbWrite)
+        return ""
     }
 
     try app.register(collection: TodoController())
