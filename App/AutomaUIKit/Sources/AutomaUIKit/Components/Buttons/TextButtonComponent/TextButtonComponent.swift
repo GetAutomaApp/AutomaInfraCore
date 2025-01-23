0849204635b171f3d7bb2475d7af9a32ed2b5772
AdonisCodes <business@simonferns.com>
2025-01-23 06:12:15 -0800

    simon/onboarding-v2

---
+++ App/AutomaUIKit/Sources/AutomaUIKit/Components/Buttons/TextButtonComponent/TextButtonComponent.swift
@@ -0,0 +1,94 @@
+// TextButtonComponent.swift
+// Copyright (c) 2025 GetAutomaApp
+// All source code and related assets are the property of GetAutomaApp.
+// All rights reserved.
+
+import SwiftUI
+
+/// A custom button component that displays an icon and allows for flexible configuration and action handling.
+public struct TextButtonComponent: View {
+    /// The external configuration of the button, provided by the parent view.
+    @ObservedObject private var externalConfig: TextButtonComponentConfig
+
+    /// The configuration to use for the button. This is either the external or internal config.
+    private var config: TextButtonComponentConfig { externalConfig }
+
+    /// A closure that is called when the button appears on screen, passing the current configuration.
+    let onSelfAppear: (TextButtonComponentConfig) -> Void
+
+    /// A closure to execute when the button is tapped, passing the current configuration.
+    let action: (TextButtonComponentConfig) -> Void
+
+    // MARK: - 1. Initializer with external config
+
+    /// Initializes the TextButtonComponent with an external configuration.
+    ///
+    /// - Parameters:
+    ///   - config: The external configuration that defines the button's appearance and behavior.
+    ///   - onSelfAppear: A closure called when the button appears on screen (default is no-op).
+    ///   - action: A closure called when the button is tapped (default is no-op).
+    public init(
+        config: TextButtonComponentConfig,
+        onSelfAppear: @escaping (TextButtonComponentConfig) -> Void = { _ in },
+        action: @escaping (TextButtonComponentConfig) -> Void = { _ in }
+    ) {
+        externalConfig = config
+        self.onSelfAppear = onSelfAppear
+        self.action = action
+    }
+
+    // MARK: - 2. Initializer with default internal config
+
+    /// Initializes the TextButtonComponent with a default internal configuration.
+    ///
+    /// - Parameters:
+    ///   - onSelfAppear: A closure called when the button appears on screen (default is no-op).
+    ///   - defaultText: The default text the button should display
+    ///   - action: A closure called when the button is tapped (default is no-op).
+    public init(
+        onSelfAppear: @escaping (TextButtonComponentConfig) -> Void = { _ in },
+        defaultText: String,
+        action: @escaping (TextButtonComponentConfig) -> Void = { _ in }
+    ) {
+        self.onSelfAppear = onSelfAppear
+        self.action = action
+
+        _externalConfig = .init(initialValue: .init())
+        externalConfig.text = defaultText
+    }
+
+    // MARK: - 3. Initializer with action without config
+
+    /// Initializes the TextButtonComponent with a closure for action, but no external configuration.
+    ///
+    /// - Parameters:
+    ///   - onSelfAppear: A closure called when the button appears on screen (default is no-op).
+    ///   - defaultText: The default text the button should display
+    ///   - action: A closure called when the button is tapped (default is no-op).
+    public init(
+        onSelfAppear: @escaping (TextButtonComponentConfig) -> Void = { _ in },
+        defaultText: String,
+        action: @escaping () -> Void = {}
+    ) {
+        self.init(
+            onSelfAppear: onSelfAppear,
+            defaultText: defaultText,
+            action: { _ in action() }
+        )
+    }
+
+    // MARK: - Body
+
+    /// The view body that represents the button. It renders a button frame with the specified text
+    ///
+    /// - Returns: A Button view wrapped in a custom frame, displaying the text and triggering the associated action.
+    public var body: some View {
+        ButtonFrameComponent(config: config, action: {
+            action(config)
+        }) {
+            Text(config.text).fontTableFont(FontTable.SFPro.Headings.head6)
+        } onSelfAppear: { _ in
+            onSelfAppear(config)
+        }
+    }
+}
---
+++ App/AutomaUIKit/Sources/AutomaUIKit/Components/Buttons/TextButtonComponent/TextButtonComponentConfig.swift
@@ -0,0 +1,78 @@
+// TextButtonComponentConfig.swift
+// Copyright (c) 2025 GetAutomaApp
+// All source code and related assets are the property of GetAutomaApp.
+// All rights reserved.
+
+import SwiftUI
+
+/// Enum defining the different variants of the TextButton component.
+public enum TextButtonVariants: String, CaseIterable {
+    case generic
+    case square
+    case circle
+    case pill
+}
+
+/// Configuration for the TextButtonComponent that extends from `ButtonFrameComponentConfig`.
+/// It defines the visual style, state (enabled/disabled), and the default text.
+public class TextButtonComponentConfig: ButtonFrameComponentConfig {
+    /// The variant of the button (e.g., generic, square, circle, pill).
+    /// This controls the button's shape and layout style.
+    @Published public var variant: TextButtonVariants = .generic {
+        didSet {
+            applyVariantStyling()
+        }
+    }
+
+    /// Whether the button is disabled or not. When disabled, the button appears inactive.
+    @Published var isDisabled: Bool = false {
+        didSet {
+            manageDisabledState()
+        }
+    }
+
+    /// The text to display on the button. This defines the visual text that the button will use
+    @Published public var text: String = "Enter Text Here"
+
+    /// Initializes the `TextButtonComponentConfig` with default styling.
+    ///
+    /// This calls the superclass's initializer and applies the default styling for the button's variant.
+    public init() {
+        super.init()
+        applyVariantStyling()
+    }
+
+    /// Applies the appropriate styling based on the selected variant.
+    ///
+    /// This method adjusts properties like `isCircular`, `roundness`, `fillSpace`, and `defaultPadding`
+    /// based on the button's variant (generic, square, circle, or pill).
+    func applyVariantStyling() {
+        switch variant {
+        case .generic:
+            isCircular = false
+            roundness = DesignTokens.defaultCornerRadius
+            fillSpace = true
+        case .square:
+            isCircular = false
+            roundness = DesignTokens.defaultCornerRadius
+            fillSpace = false
+            defaultPadding = DesignTokens.padding.buttonEven
+        case .circle:
+            isCircular = true
+            fillSpace = false
+            defaultPadding = DesignTokens.padding.buttonEven
+        case .pill:
+            isCircular = true
+            fillSpace = true
+            defaultPadding = DesignTokens.padding.button
+        }
+    }
+
+    /// Manages the disabled state of the button.
+    ///
+    /// When the button is disabled (`isDisabled = true`), the button's frame variant is set to `.disabled`.
+    /// If the button is not disabled, it uses the `.generic` frame variant.
+    func manageDisabledState() {
+        frameVariant = isDisabled ? .disabled : .generic
+    }
+}
