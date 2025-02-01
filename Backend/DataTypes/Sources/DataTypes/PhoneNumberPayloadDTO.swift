0cd65d241074006ff5c482f02c0111596ea7c087
AdonisCodes <business@simonferns.com>
2025-02-01 11:54:51 -0800

    feature: add phone number verification

--- Backend/DataTypes/Sources/DataTypes/PhoneNumberPayloadDTO.swift
+++ Backend/DataTypes/Sources/DataTypes/PhoneNumberPayloadDTO.swift
@@ -3,12 +3,20 @@
 // All source code and related assets are the property of GetAutomaApp.
 // All rights reserved.
 
+import PhoneNumberKit
 import Vapor
 
 public struct PhoneNumberPayloadDTO: Content {
     public var phoneNumber: String
 
-    public init(phoneNumber: String) {
-        self.phoneNumber = phoneNumber
+    public init(phoneNumber: String) throws {
+        let phoneNumberKit = PhoneNumberKit()
+        do {
+            let phoneNumberParsed = try phoneNumberKit.parse(phoneNumber)
+            self.phoneNumber = phoneNumberKit
+                .format(phoneNumberParsed, toType: .e164)
+        } catch {
+            throw GenericErrors.invalidPhoneNumber
+        }
     }
 }
