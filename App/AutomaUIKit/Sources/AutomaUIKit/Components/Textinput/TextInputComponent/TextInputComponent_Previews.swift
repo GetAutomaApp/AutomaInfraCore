ae6977d7f798de0e689aaa07b0f11f055f42228d
AdonisCodes <business@simonferns.com>
2025-02-13 08:05:33 -0700

    fix: rename "ComponentComponent" -> "Component"

--- App/AutomaUIKit/Sources/AutomaUIKit/Components/Textinput/TextInputComponent/TextInputComponent_Previews.swift
+++ App/AutomaUIKit/Sources/AutomaUIKit/Components/Textinput/TextInputComponent/TextInputComponent_Previews.swift
@@ -1,4 +1,4 @@
-// TextInputComponentComponent_Previews.swift
+// TextInputComponent_Previews.swift
 // Copyright (c) 2025 GetAutomaApp
 // All source code and related assets are the property of GetAutomaApp.
 // All rights reserved.
