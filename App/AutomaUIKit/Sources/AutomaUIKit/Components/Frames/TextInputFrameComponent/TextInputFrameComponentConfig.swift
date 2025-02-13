c79b13db8a2b15ca5cad31973a47098e4c172345
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2025-02-13 07:19:58 -0700

    ui/finish text frame implementation (#94)
    * feature: implement hacky property editor mask (get set bindings)
    
    * feature: add current & disabled background colors to text input frame

--- App/AutomaUIKit/Sources/AutomaUIKit/Components/Frames/TextInputFrameComponent/TextInputFrameComponentConfig.swift
+++ App/AutomaUIKit/Sources/AutomaUIKit/Components/Frames/TextInputFrameComponent/TextInputFrameComponentConfig.swift
@@ -5,8 +5,9 @@
 
 import SwiftUI
 
-public enum TextInputFrameComponentVariants {
+public enum TextInputFrameComponentVariants: String, CaseIterable {
     case generic
+    case disabled
 }
 
 public class TextInputFrameComponentConfig: ObservableObject {
@@ -14,20 +15,50 @@ public class TextInputFrameComponentConfig: ObservableObject {
     @Published public var ghostText: String
     @Published public var icon: DesignIcons
     @Published public var hasIcon: Bool
+    @Published public var backgroundColor: Color
+    @Published public var currentBackgroundColor: Color
+    @Published public var disabledBackgroundColor: Color
+    @Published public var textColor: Color
+    @Published public var padding: EdgeInsets
+    @Published public var cornerRadius: CGSize
 
-    public var variant: TextInputFrameComponentVariants
+    @Published public var variant: TextInputFrameComponentVariants {
+        willSet { newValue
+            switch newValue {
+            case .generic:
+                currentBackgroundColor = backgroundColor
+            case .disabled:
+                currentBackgroundColor = disabledBackgroundColor
+            }
+        }
+    }
+
+    var isDisabled: Bool {
+        variant == .disabled
+    }
 
     public init(
         text: String = "",
         ghostText: String = "Hello There!",
         icon: DesignIcons = .arrowRight,
         hasIcon: Bool = true,
-        variant: TextInputFrameComponentVariants = .generic
+        variant: TextInputFrameComponentVariants = .generic,
+        backgroundColor: Color = DesignTokens.colors.primaryWhitespace2,
+        disabledBackgroundColor: Color = DesignTokens.colors.primaryWhitespace3,
+        textColor: Color = DesignTokens.colors.primaryText,
+        padding: EdgeInsets = DesignTokens.padding.smallPaddingVar,
+        cornerRadius: CGSize = DesignTokens.padding.cornerRadiusBase
     ) {
         self.text = text
         self.ghostText = ghostText
         self.icon = icon
         self.hasIcon = hasIcon
         self.variant = variant
+        self.backgroundColor = backgroundColor
+        self.disabledBackgroundColor = disabledBackgroundColor
+        currentBackgroundColor = backgroundColor
+        self.textColor = textColor
+        self.padding = padding
+        self.cornerRadius = cornerRadius
     }
 }
