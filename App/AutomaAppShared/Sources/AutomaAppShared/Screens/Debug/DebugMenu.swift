b02ce3d0cd497b76029b9c9b43a7b137fbf3a117
AdonisCodes <business@simonferns.com>
2025-02-24 05:54:00 -0700

    feat: implement base api url to app

--- App/AutomaAppShared/Sources/AutomaAppShared/Screens/Debug/DebugMenu.swift
+++ App/AutomaAppShared/Sources/AutomaAppShared/Screens/Debug/DebugMenu.swift
@@ -9,12 +9,27 @@
 //
 //  Created by Simon Ferns on 12/16/24.
 //
+
 import AutomaUIKit
 import SwiftUI
 
+enum BaseEnvironmentUrl: String, CaseIterable {
+    case sandbox = "https://api-sandbox.getautoma.app"
+    case production = "https://api-production.getautoma.app"
+    case staging = "https://api-staging.getautoma.app"
+    case localhost = "http://localhost:8080"
+}
+
 public struct DebugMenu: View {
     @EnvironmentObject var baseEnvironmentConfig: BaseAppEnvironmentObject
+
     @StateObject var closeButtonConfig: IconButtonComponentConfig = .init()
+
+    @StateObject var environmentPickerConfig: TextInputFrameComponentConfig = .init(
+        text: BaseEnvironmentUrl.sandbox.rawValue
+    )
+
+    @StateObject var environmentPicketButtonConfig: TextButtonComponentConfig = .init()
 
     public init() {}
 
@@ -42,11 +57,55 @@ public struct DebugMenu: View {
                     )
                 }
             }
+
             Section(header: Text("Profile Actions")) {
                 Text("🍃 Log Out")
             }.onTapGesture {
                 baseEnvironmentConfig.logout()
                 baseEnvironmentConfig.isDebugMenuActive = false
+            }
+
+            Section(header: Text("Api Configuration")) {
+                VStack {
+                    Picker(
+                        "API URL",
+                        selection: $environmentPickerConfig.text
+                    ) {
+                        ForEach(
+                            BaseEnvironmentUrl.allCases,
+                            id: \.rawValue
+                        ) { url in
+                            Text(url.rawValue)
+                        }
+                    }
+
+                    TextInputFrameComponent(
+                        config: environmentPickerConfig,
+                        onSelfAppear: {
+                            environmentPickerConfig.text = baseEnvironmentConfig.apiBaseURL
+                        }
+                    )
+
+                    TextButtonComponent(
+                        config: environmentPicketButtonConfig,
+                        onSelfAppear: { config in
+                            config.text = "Set URL"
+                        },
+                        action: { _ in
+                            let url = URL(string: environmentPickerConfig.text)
+
+                            if let url {
+                                baseEnvironmentConfig.apiBaseURL = url.absoluteString
+                                baseEnvironmentConfig.logout()
+                                baseEnvironmentConfig.isDebugMenuActive = false
+                            } else {
+                                print(
+                                    "HANDLE ERRORS INVALID URL \(environmentPickerConfig.text)"
+                                )
+                            }
+                        }
+                    )
+                }
             }
         }
         Spacer()
