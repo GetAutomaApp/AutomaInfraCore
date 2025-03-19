1c2889166d953771bf23c68ce48a7ec2ee230c9f
William <vimscientist69@gmail.com>
2025-03-19 03:31:45 -0600

    Squash merge develop into tests-chatcompletion

--- Backend/Sources/App/Clients/ChatCompletionClient/ChatCompletionClientTypes.swift
+++ Backend/Sources/App/Clients/ChatCompletionClient/ChatCompletionClientTypes.swift
@@ -5,6 +5,20 @@
 
 import Vapor
 
+protocol ChatCompletion {
+    var logger: Logger { get }
+    var supportedModels: [String] { get }
+    func createChat(_ query: ChatCompletionContent) async throws -> ChatCompletionResult
+}
+
+extension ChatCompletion {
+    func validateModel(model: ChatCompletionModel) throws {
+        guard supportedModels.contains(model.rawValue) else {
+            throw Abort(.badRequest, reason: "Invalid model for OpenAI platform")
+        }
+    }
+}
+
 struct ChatCompletionContent: Content {
     let model: ChatCompletionModel
     let prompt: String
