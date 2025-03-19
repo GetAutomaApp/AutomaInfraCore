f91488bdf68911988b9d856b524e4d42050dbb44
William <vimscientist69@gmail.com>
2025-03-19 03:31:25 -0600

    refactor(ChatCompletionClient): moved types to types module

--- Backend/Sources/App/Clients/ChatCompletionClient/ChatCompletionClient.swift
+++ Backend/Sources/App/Clients/ChatCompletionClient/ChatCompletionClient.swift
@@ -3,18 +3,3 @@
 // All source code and related assets are the property of GetAutomaApp.
 // All rights reserved.
 
-import Vapor
-
-protocol ChatCompletion {
-    var logger: Logger { get }
-    var supportedModels: [String] { get }
-    func createChat(_ query: ChatCompletionContent) async throws -> ChatCompletionResult
-}
-
-extension ChatCompletion {
-    func validateModel(model: ChatCompletionModel) throws {
-        guard supportedModels.contains(model.rawValue) else {
-            throw Abort(.badRequest, reason: "Invalid model for OpenAI platform")
-        }
-    }
-}
