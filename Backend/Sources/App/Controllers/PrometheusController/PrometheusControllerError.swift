0590d9a139c800e41b3933b1ebfd8d0a8a9c322d
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2025-03-30 11:17:02 -0600

    use opensource-actions swiftlint action instead (#157)
    * fix: lint entire codebase
    
    * testing
    
    * chore: remove install swiftlint (we're using custom actio now)
    
    * fix: use checkout v4

--- Backend/Sources/App/Controllers/PrometheusController/PrometheusControllerError.swift
+++ Backend/Sources/App/Controllers/PrometheusController/PrometheusControllerError.swift
@@ -1,3 +1,8 @@
+// PrometheusControllerError.swift
+// Copyright (c) 2025 GetAutomaApp
+// All source code and related assets are the property of GetAutomaApp.
+// All rights reserved.
+
 enum PrometheusControllerError: Error {
     case couldNotConvertMetricsToData
     case invalidAuthToken
