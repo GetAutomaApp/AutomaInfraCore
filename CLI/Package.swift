26a1767170a61febf1fa45573f90fe4c0ce3c0cf
AdonisCodes <business@simonferns.com>
2025-04-12 09:42:46 -0600

    feat: fix stuff

--- CLI/Package.swift
+++ CLI/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.1
 import PackageDescription
 
 let package = Package(
@@ -9,15 +9,12 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
-        // 🗄 An ORM for SQL and NoSQL databases.
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
         .package(path: "../Backend/DataTypes"),
     ],
     targets: [
         .executableTarget(
             name: "AutomaCLI",
             dependencies: [
-                .product(name: "Fluent", package: "fluent"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "DataTypes", package: "DataTypes"),
             ],
