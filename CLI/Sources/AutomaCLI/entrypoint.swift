d4764a3fe65fdc0cd6de2bcdaa9040cfb0e8c50b
AdonisCodes <business@simonferns.com>
2025-04-14 07:56:56 -0600

    swift changes

--- CLI/Sources/AutomaCLI/entrypoint.swift
+++ CLI/Sources/AutomaCLI/entrypoint.swift
@@ -10,7 +10,6 @@ enum Entrypoint {
     static func main() async throws {
         let env = try Environment.detect()
         let app = try await Application.make(env)
-
         defer { Task { try? await app.asyncShutdown() } }
         app.commands.use(GenerateAppComponent(), as: "generate")
         app.commands.use(FlyConfigGenerator(), as: "fly-config")
