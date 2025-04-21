1305337162b2bc3ee910b84cc0c42535c7efaa74
AdonisCodes <business@simonferns.com>
2025-04-21 08:08:14 -0600

    feat: experiment xcbuild tests

--- App/AutomaApp/Admin/IOS/IOS-ADMINTests/IOS_ADMINTests.swift
+++ App/AutomaApp/Admin/IOS/IOS-ADMINTests/IOS_ADMINTests.swift
@@ -9,5 +9,6 @@ import Testing
 struct IOS_ADMINTests {
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // MOCK TESTS
     }
 }
