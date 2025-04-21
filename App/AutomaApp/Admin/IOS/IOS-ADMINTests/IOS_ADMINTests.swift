1b15af2df2a4986522ca4d81a1d4e43d18db2635
AdonisCodes <business@simonferns.com>
2025-04-21 08:14:00 -0600

    feat: fix ios tests

--- App/AutomaApp/Admin/IOS/IOS-ADMINTests/IOS_ADMINTests.swift
+++ App/AutomaApp/Admin/IOS/IOS-ADMINTests/IOS_ADMINTests.swift
@@ -9,6 +9,5 @@ import Testing
 struct IOS_ADMINTests {
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        // MOCK TESTS
     }
 }
