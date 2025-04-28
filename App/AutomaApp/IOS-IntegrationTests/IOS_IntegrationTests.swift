0f5bc225f17115c3d4a9bf78c9f06f42cd163a4a
AdonisCodes <business@simonferns.com>
2025-04-28 08:30:24 -0600

    fix: integration testing bundle

---
+++ App/AutomaApp/IOS-IntegrationTests/IOS_IntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  IOS_IntegrationTests.swift
+//  IOS-IntegrationTests
+//
+//  Created by Simon Ferns on 4/28/25.
+//
+
+import Testing
+
+struct IOS_IntegrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
