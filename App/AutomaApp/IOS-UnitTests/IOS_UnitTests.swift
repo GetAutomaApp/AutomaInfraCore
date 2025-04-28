81d062fcdb3360987bf816b1a3f7d678f0cbf6e6
AdonisCodes <business@simonferns.com>
2025-04-28 08:26:14 -0600

    fix: add ios core unit tests

---
+++ App/AutomaApp/IOS-UnitTests/IOS_UnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  IOS_UnitTests.swift
+//  IOS-UnitTests
+//
+//  Created by Simon Ferns on 4/28/25.
+//
+
+import Testing
+
+struct IOS_UnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
