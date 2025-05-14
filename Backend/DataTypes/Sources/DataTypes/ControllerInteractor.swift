eb2037f4ea534c000e28093fa8b5cc8477286675
William <vimscientist69@gmail.com>
2025-05-14 10:41:04 +0200

    fix(ControllerInteractor): public declaration cannot be used in protocols

--- Backend/DataTypes/Sources/DataTypes/ControllerInteractor.swift
+++ Backend/DataTypes/Sources/DataTypes/ControllerInteractor.swift
@@ -31,7 +31,7 @@ public protocol ControllerInteractor {
     ///
     /// - Throws: An error if the request fails to execute
     /// - Returns: An Alamofire DataResponse object containing the response data and any errors
-    public func performRequest(
+    func performRequest(
         endpoint: String,
         method: Alamofire.HTTPMethod,
         headers: Alamofire.HTTPHeaders?,
@@ -110,7 +110,7 @@ public protocol BackendControllerInteractor: ControllerInteractor {
     ///   - decodeTo: The expected type to decode the response into
     ///
     /// - Returns: A BackendControllerResponseOutput containing either the decoded data or an error
-    public func decodeResponse<K: Content>(
+    func decodeResponse<K: Content>(
         _ data: DataResponse<Data?, AFError>,
         _ decodeTo: K.Type
     ) -> BackendControllerResponseOutput<K>
