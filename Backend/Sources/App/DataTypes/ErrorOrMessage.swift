0fcf3b5715108fe38ae941932ca617366614031c
William Ferns <vimscientist69@gmail.com>
2025-05-29 15:36:44 +0200

    refactor(helpers.swift): moved all helper types and extensions to their own files (#183)

---
+++ Backend/Sources/App/DataTypes/ErrorOrMessage.swift
@@ -0,0 +1,9 @@
+// ErrorOrMessage.swift
+// Copyright (c) 2025 GetAutomaApp
+// All source code and related assets are the property of GetAutomaApp.
+// All rights reserved.
+
+internal enum ErrorOrMessage {
+    case error(Error)
+    case message(String)
+}
---
+++ Backend/Sources/App/Extensions/Foundation/UUIDExtensions.swift
@@ -0,0 +1,18 @@
+// UUIDExtensions.swift
+// Copyright (c) 2025 GetAutomaApp
+// All source code and related assets are the property of GetAutomaApp.
+// All rights reserved.
+
+import Vapor
+
+public extension UUID {
+    static func unwrapFromString(_ uuidString: String, _ callback: () throws -> Void) throws -> UUID {
+        guard
+            let uuid = UUID(uuidString: uuidString)
+        else {
+            try callback()
+            throw Abort(.internalServerError)
+        }
+        return uuid
+    }
+}
