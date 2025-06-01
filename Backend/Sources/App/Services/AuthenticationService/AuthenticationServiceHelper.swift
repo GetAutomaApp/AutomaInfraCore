636494c17f6b9851ebef6f4b84dd0c7c3cd3bc56
William <vimscientist69@gmail.com>
2025-06-01 15:49:44 +0200

    refactor(AuthenticationServiceHelper): refactored `deleteOldTokens`

--- Backend/Sources/App/Services/AuthenticationService/AuthenticationServiceHelper.swift
+++ Backend/Sources/App/Services/AuthenticationService/AuthenticationServiceHelper.swift
@@ -95,8 +95,6 @@ actor AuthenticationServiceHelper {
         )).send()
     }
 
-    // TODO: cleanup, don't throw error at top level
-    // (have name be `logAuthCodeNotSent`)
     public func sendTelemetryDataOnAuthCodeSendFail(
         code: String,
         phoneNumber: String,
@@ -217,6 +215,17 @@ struct AuthenticationServiceHelperConfig: AuthenticationServiceConfig {
 internal struct DeleteOldAccessTokensPayload {
     let userId: UUID
     let subject: JWTTokenSubject
+    let totalNewestTokensToSkip: Int?
+
+    init(
+        userId: UUID,
+        subject: JWTTokenSubject,
+        totalNewestTokensToSkip: Int? = nil
+    ) {
+        self.userId = userId
+        self.subject = subject
+        self.totalNewestTokensToSkip = totalNewestTokensToSkip
+    }
 }
 
 internal struct AccessTokenResetter {
@@ -235,9 +244,13 @@ internal struct AccessTokenResetter {
                 writeDb: config.writeDb,
                 readDb: config.readDb,
                 logger: config.logger,
-                payload: .init(userId: config.payload.userId, subject: config.payload.subject)
+                payload: .init(
+                    userId: config.payload.userId,
+                    subject: config.payload.subject,
+                    totalNewestTokensToSkip: 5
+                )
             )
-        ).deleteOldTokens(skip: 5)
+        ).deleteOldTokens()
         try await createAuthToken(fromSignedToken: signedToken)
         return signedToken
     }
@@ -287,44 +300,74 @@ internal struct OldAccessTokenDeleter {
         self.config = config
     }
 
-    // TODO: refactor
-    public func deleteOldTokens(
-        skip: Int? = nil
-    ) async throws {
-        let subject = config.payload.subject
-        let userId = config.payload.userId
+    public func deleteOldTokens() async throws {
+        logDeleteOldTokensStart()
+        try await deleteTokens(getTokensToDelete())
+    }
 
+    private func deleteTokens(_ tokens: [JwtTokenModel]) async throws {
+        try await withThrowingTaskGroup(of: Void.self) { group in
+            for token in tokens {
+                group.addTask {
+                    try await deleteToken(token)
+                }
+            }
+
+            try await group.waitForAll()
+        }
+    }
+
+    private func deleteToken(_ token: JwtTokenModel) async throws {
+        try await token.delete(on: config.writeDb)
+        try logDeleteTokenSuccess(id: token.requireID())
+    }
+
+    private func logDeleteTokenSuccess(id: UUID) {
+        config.logger.info(
+            "Deleted token",
+            metadata: [
+                "to": .string("\(String(describing: Self.self)).\(#function)"),
+                "tokenId": .string(id.uuidString)
+            ]
+        )
+    }
+
+    private func getTokensToDelete() async throws -> [JwtTokenModel] {
+        try await getQueryForTokensToDelete().all()
+    }
+
+    private func getQueryForTokensToDelete() -> QueryBuilder<JwtTokenModel> {
+        var query = getQueryForAllTokensInDescendingOrder()
+        if let totalNewestTokensToSkip = config.payload.totalNewestTokensToSkip {
+            query = skipSomeNewestTokens(amount: totalNewestTokensToSkip, fromQuery: query)
+        }
+        return query
+    }
+
+    private func skipSomeNewestTokens(
+        amount: Int,
+        fromQuery query: QueryBuilder<JwtTokenModel>
+    ) -> QueryBuilder<JwtTokenModel> {
+        query.range(amount...)
+    }
+
+    private func getQueryForAllTokensInDescendingOrder() -> QueryBuilder<JwtTokenModel> {
+        JwtTokenModel.query(on: config.writeDb)
+            .filter(\.$userId == config.payload.userId)
+            .filter(\.$subject == config.payload.subject)
+            .sort(\.$createdAt, .descending)
+    }
+
+    private func logDeleteOldTokensStart() {
         config.logger.info(
             "Deleting old tokens",
             metadata: [
                 "to": .string("\(String(describing: Self.self)).\(#function)"),
-                "userId": .string(userId.uuidString),
-                "subject": .string(String(reflecting: subject)),
-                "skip": .string(String(reflecting: skip)),
+                "userId": .string(config.payload.userId.uuidString),
+                "subject": .string(String(reflecting: config.payload.subject)),
+                "skip": .string(String(reflecting: config.payload.totalNewestTokensToSkip)),
             ]
         )
-
-        var query = JwtTokenModel.query(on: config.writeDb)
-            .filter(\.$userId == userId)
-            .filter(\.$subject == subject)
-            .sort(\.$createdAt, .descending)
-
-        if let skip {
-            query = query.range(skip...)
-        }
-
-        let tokensToDelete = try await query.all()
-
-        for token in tokensToDelete {
-            try await token.delete(on: config.writeDb)
-            config.logger.info(
-                "Deleted token",
-                metadata: [
-                    "to": .string("AuthenticationService.deleteOldTokens"),
-                    "tokenId": .string(token.id?.uuidString ?? "unknown"),
-                ]
-            )
-        }
     }
 }
 
