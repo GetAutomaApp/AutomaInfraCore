df33d01b5de1e89a8367d22b3e72600c4e4cd488
William <vimscientist69@gmail.com>
2025-10-02 12:46:44 +0200

    fix(BackendMetric): use metric status data type from automa utilities

--- Backend/Sources/App/DataTypes/BackendMetric.swift
+++ Backend/Sources/App/DataTypes/BackendMetric.swift
@@ -12,56 +12,56 @@ internal enum BackendMetric {
     /// Counter for successful verification codes sent.
     public static let totalSuccessfulVerificationCodesSent = MetricsService.global.makeCounter(
         name: "total_verification_codes_sent",
-        labels: ["status": Self.MetricStatus.success.rawValue]
+        labels: ["status": MetricStatus.success.rawValue]
     )
 
     /// Counter for failed verification codes sent.
     public static let totalFailedVerificationCodesSent = MetricsService.global.makeCounter(
         name: "total_verification_codes_sent",
-        labels: ["status": Self.MetricStatus.fail.rawValue]
+        labels: ["status": MetricStatus.fail.rawValue]
     )
 
     /// Counter for total users created.
     public static let totalUsersCreated = MetricsService.global.makeCounter(
         name: "total_users_created",
-        labels: ["status": Self.MetricStatus.success.rawValue]
+        labels: ["status": MetricStatus.success.rawValue]
     )
 
     /// Counter for users that already exist.
     public static let totalUsersAlreadyExists = MetricsService.global.makeCounter(
         name: "total_users_created",
-        labels: ["status": Self.MetricStatus.alreadyExists.rawValue]
+        labels: ["status": MetricStatus.alreadyExists.rawValue]
     )
 
     /// Counter for successful token refresh attempts.
     public static let totalSuccessfulTokensRefreshed = MetricsService.global.makeCounter(
         name: "total_token_refresh_attempts",
-        labels: ["status": Self.MetricStatus.success.rawValue]
+        labels: ["status": MetricStatus.success.rawValue]
     )
 
     /// Counter for failed token refresh attempts.
     public static let totalFailedTokensRefreshed = MetricsService.global.makeCounter(
         name: "total_token_refresh_attempts",
-        labels: ["status": Self.MetricStatus.fail.rawValue]
+        labels: ["status": MetricStatus.fail.rawValue]
     )
 
     /// Counter for logout attempts.
     public static let totalLogoutAttempted = MetricsService.global.makeCounter(
         name: "total_logout_attempts",
-        labels: ["status": Self.MetricStatus.success.rawValue]
+        labels: ["status": MetricStatus.success.rawValue]
     )
 
     /// Counter for failed logout attempts.
     public static let totalFailedLogoutAttempted = MetricsService.global.makeCounter(
         name: "total_logout_attempts",
-        labels: ["status": Self.MetricStatus.fail.rawValue]
+        labels: ["status": MetricStatus.fail.rawValue]
     )
 
     /// Counter for profile pictures generated.
     public static let totalProfilePicturesGenerated = MetricsService.global.makeCounter(
         name: "total_profile_pictures_generated",
         labels: [
-            "status": Self.MetricStatus.success.rawValue,
+            "status": MetricStatus.success.rawValue,
         ]
     )
 
@@ -69,7 +69,7 @@ internal enum BackendMetric {
     public static let totalProfilePicturesGenerationFailed = MetricsService.global.makeCounter(
         name: "total_profile_pictures_generated",
         labels: [
-            "status": Self.MetricStatus.fail.rawValue,
+            "status": MetricStatus.fail.rawValue,
         ]
     )
 
@@ -77,7 +77,7 @@ internal enum BackendMetric {
     public static let totalTextMessagesSent = MetricsService.global.makeCounter(
         name: "total_text_messages_sent",
         labels: [
-            "status": Self.MetricStatus.success.rawValue,
+            "status": MetricStatus.success.rawValue,
         ]
     )
 
@@ -85,7 +85,7 @@ internal enum BackendMetric {
     public static let totalTextMessagesSentFailed = MetricsService.global.makeCounter(
         name: "total_text_messages_sent",
         labels: [
-            "status": Self.MetricStatus.fail.rawValue,
+            "status": MetricStatus.fail.rawValue,
         ]
     )
 
@@ -98,7 +98,7 @@ internal enum BackendMetric {
     /// - Parameter status: The status of the request.
     /// - Returns: A `Prometheus.Counter` object.
     public static func openAIImageGenerationRequest(
-        status: Self.MetricStatus
+        status: MetricStatus
     ) -> Prometheus.Counter {
         MetricsService.global.makeCounter(
             name: "openai_image_generation_requests",
@@ -112,7 +112,7 @@ internal enum BackendMetric {
     /// - Parameter status: The status of the OAuth request (success/fail/etc).
     /// - Returns: A Prometheus counter for Twitter OAuth requests with the given status.
     public static func twitterOAuthRequest(
-        status: Self.MetricStatus
+        status: MetricStatus
     ) -> Prometheus.Counter {
         MetricsService.global.makeCounter(
             name: "twitter_oauth_requests",
@@ -126,7 +126,7 @@ internal enum BackendMetric {
     /// - Parameter status: The status of the token conversion (success/fail/etc).
     /// - Returns: A Prometheus counter for Twitter token conversions with the given status.
     public static func twitterUserTokensConverted(
-        status: Self.MetricStatus
+        status: MetricStatus
     ) -> Prometheus.Counter {
         MetricsService.global.makeCounter(
             name: "twitter_user_tokens_converted",
@@ -140,7 +140,7 @@ internal enum BackendMetric {
     /// - Parameter status: The status of posting the tweet (success/fail/etc).
     /// - Returns: A Prometheus counter for tweet posts with the given status.
     public static func twitterPostTweet(
-        status: Self.MetricStatus
+        status: MetricStatus
     ) -> Prometheus.Counter {
         MetricsService.global.makeCounter(
             name: "twitter_post_tweet",
@@ -158,7 +158,7 @@ internal enum BackendMetric {
     ///   - didThrowOnFeedInitialization: Flag indicating if an error occurred during feed initialization.
     /// - Returns: A Prometheus counter for RSS feed reads with the given status.
     public static func rssFeedReaderMetric(
-        status: Self.MetricStatus,
+        status: MetricStatus,
         url: URL,
         isRSSFeed: Bool? = nil,
         didThrowOnFeedInitialization: Bool = false
@@ -185,7 +185,7 @@ internal enum BackendMetric {
     public static func chatCompletionServiceCall(
         platform: ChatCompletionPlatform,
         model: ChatCompletionModel,
-        status: Self.MetricStatus
+        status: MetricStatus
     ) -> Prometheus.Counter {
         MetricsService.global.makeCounter(
             name: "chat_completion_service_call",
@@ -201,7 +201,7 @@ internal enum BackendMetric {
     public static let totalMediaFilesUploadedToTigris = MetricsService.global.makeCounter(
         name: "total_media_files_uploaded_to_tigris",
         labels: [
-            "status": Self.MetricStatus.success.rawValue,
+            "status": MetricStatus.success.rawValue,
         ]
     )
 
@@ -209,7 +209,7 @@ internal enum BackendMetric {
     public static let totalMediaFilesUploadedToTigrisFailed = MetricsService.global.makeCounter(
         name: "total_media_files_uploaded_to_tigris",
         labels: [
-            "status": Self.MetricStatus.fail.rawValue,
+            "status": MetricStatus.fail.rawValue,
         ]
     )
 
@@ -219,7 +219,7 @@ internal enum BackendMetric {
     ///   - url: The URL being scraped.
     /// - Returns: A Prometheus counter for Firecrawl markdown scraping with the given status.
     public static func firecrawlScrapeMarkdown(
-        status: Self.MetricStatus,
+        status: MetricStatus,
         url: String
     ) -> Prometheus.Counter {
         MetricsService.global.makeCounter(
@@ -229,17 +229,5 @@ internal enum BackendMetric {
                 "url": url,
             ]
         )
-    }
-
-    /// Enum representing the status of a metric.
-    public enum MetricStatus: String, Codable {
-        /// Indicates that the metric already exists.
-        case alreadyExists
-        /// Indicates a failure status for the metric.
-        case fail
-        /// Indicates a start status for the metric.
-        case start
-        /// Indicates a success status for the metric.
-        case success
     }
 }
