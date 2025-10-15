67da912766c382e38f6332073dd99a3009f18a24
William <vimscientist69@gmail.com>
2025-10-15 06:06:34 +0200

    refactor(ArticleContentScraperService): converted long public method to use multiple helper methods at same abstraction level to improve cleanliness

--- Backend/Sources/App/Services/ArticleContentScraperService/ArticleContentScraperService.swift
+++ Backend/Sources/App/Services/ArticleContentScraperService/ArticleContentScraperService.swift
@@ -13,34 +13,13 @@ internal struct ArticleContentScraperService {
     internal let logger: Logger
 
     public func getArticle(payload: AutomaWebCoreAPIEndpointPayload) async throws -> Article {
-        let textContent = try await getWebsiteHTMLTextContent(payload)
-        let response = try await ChatCompletionClient(logger: logger).createChat(
-            .init(
-                model: .gpt4omini,
-                prompt: """
-                I want to convert an article to this format:
+        try await convertWebsiteHTMLTextContentToArticle(getWebsiteHTMLTextContent(payload))
+    }
 
-                {
-                    "name": "article name here",
-                    "author": "article writer/author here",
-                    "posted_at": "Date article was posted, don't provide 'posted_at' key if value not found",
-                    "content_markdown": "**full article content as markdown here**, the full article, formatted and well-punctuated; not just a section of it"
-                }
-
-                Simply return **only a json code snippet without the snippet signs (```)** in the exact format shown above.
-
-                Here is the text extracted from the article web page:
-                \(textContent)
-                """
-            )
-        ).message
-        guard
-            let articleDictData = response.data(using: .utf8)
-        else {
-            throw Abort(.internalServerError)
-        }
-
-        let article = try JSONDecoder().decode(Article.self, from: articleDictData)
+    private func convertWebsiteHTMLTextContentToArticle(_ textContent: String) async throws -> Article {
+        let jsonString = try await convertWebsiteHTMLTextContentToJSON(textContent)
+        let jsonData = try websiteContentJSONStringToData(jsonString)
+        let article = try JSONDecoder().decode(Article.self, from: jsonData)
         return article
     }
 
@@ -50,6 +29,43 @@ internal struct ArticleContentScraperService {
         let parsedWebsiteHTML = try SwiftSoup.parse(websiteHTMLString)
         let textContent = try parsedWebsiteHTML.text()
         return textContent
+    }
+
+    private func convertWebsiteHTMLTextContentToJSON(_ textContent: String) async throws -> String {
+        let response = try await ChatCompletionClient(logger: logger).createChat(
+            .init(
+                model: .gpt4omini,
+                prompt: getConvertHTMLToJSONChatCompletionPrompt(textContent: textContent)
+            )
+        ).message
+        return response
+    }
+
+    private func getConvertHTMLToJSONChatCompletionPrompt(textContent: String) -> String {
+        """
+        I want to convert an article to this format:
+
+        {
+            "name": "article name here",
+            "author": "article writer/author here",
+            "posted_at": "Date article was posted, don't provide 'posted_at' key if value not found",
+            "content_markdown": "**full article content as markdown here**, the full article, formatted and well-punctuated; not just a section of it"
+        }
+
+        Simply return **only a json code snippet without the snippet signs (```)** in the exact format shown above.
+
+        Here is the text extracted from the article web page:
+        \(textContent)
+        """
+    }
+
+    private func websiteContentJSONStringToData(_ jsonString: String) throws -> Data {
+        guard
+            let data = jsonString.data(using: .utf8)
+        else {
+            throw Abort(.internalServerError)
+        }
+        return data
     }
 
     public struct Article: Content {
